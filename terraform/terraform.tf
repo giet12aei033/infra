756f64b758d21fd7c1b9e9ede81d0906929d41d2
giet12aei033 <166718827+giet12aei033@users.noreply.github.com>
2024-04-11 18:06:02 +0530

    Adding terraform file

---
+++ terraform/terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.3.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_iam_role" "example_role" {
+  name = "example-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+output "role_arn" {
+  value = aws_iam_role.example_role.arn
+}
