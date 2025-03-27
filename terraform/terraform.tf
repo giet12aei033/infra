63e644a3abc14f6530eabb16c97dc970f0705a41
giet12aei033 <166718827+giet12aei033@users.noreply.github.com>
2025-03-28 00:31:28 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 
 resource "aws_iam_role" "example_role" {
-  name = "test-role"
+  name = "test1-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
